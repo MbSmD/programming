732286177798baf89b88abbe2e4e89981e0883de
MbSmD <144994955+MbSmD@users.noreply.github.com>
2023-11-30 14:32:31 +0500

    Add files via upload

---
+++ lab_05/grafik.py
@@ -0,0 +1,10 @@
+import matplotlib as pit
+import numpy as np 
+import matplotlib.pyplot as plt
+import math
+x = (0, 1)
+for d in x:
+    s = math.sqrt(d + 1)-math.sqrt(d) - 1**0.5
+    print(s) 
+    f = math.exp(d)
+    print(f)
